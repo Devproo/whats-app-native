c1582d6cb8553126c2cc05fd406db7196f31d61b
ipeerless <ipeerless0@gmail.com>
2022-12-19 10:13:59 +0200

    chat messages

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import ChatListItem from './src/components/ChatListItem'
+import ChatsScreen from './src/screens/ChatsScreen'
+import ChatScreen from './src/screens/ChatScreen'
+
+const chat = {
+  id: '1',
+  user: {
+    image:
+      'https://apicms.thestar.com.my/uploads/images/2022/10/11/1772200.jpg',
+    name: 'Adam',
+  },
+  lastMessage: {
+    text: 'Go',
+    createdAt: '07: 30',
+  },
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Pressable style={styles.container}>
+      <StatusBar style='auto' />
+     
+      <ChatScreen />
+    </Pressable>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 50,
   },
-});
+})
